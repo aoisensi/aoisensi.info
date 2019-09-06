a9ef726934aae1ce417d17fa42280d67678fabdc
aoisensi <aoisensi@outlook.jp>
2019-09-06 15:22:41 +0900

    index

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,30 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+    <v-app-bar
+      color="blue"
+      dark
+      app
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title class="headline">
+        <span>aoisensi.info</span>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <Index/>
     </v-content>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Index from './components/Index.vue';
 
 export default Vue.extend({
   name: 'App',
   components: {
-    HelloWorld,
+    Index,
   },
   data: () => ({
     //
